e55c2092e603e1dc4edcaf9b6453eba7dff7f35a
ziheng <zihenglv@gmail.com>
2018-03-07 15:35:50 +0800

    add: sqrt.c

--- temps/sqrt.c
+++ temps/sqrt.c
@@ -8,16 +8,18 @@
 #define myLimit(n, l) \
     ((n)*(n)-(l))
 
-double mySqrt(double n);
+double mySqrtV1(double n);
+double mySqrtV2(double n);
 
 int main(void) {
     for(int i = 0; i<17; i++){
-        printf("mySqrt(%lf) = %lf\n", (double)i, mySqrt(((double)i)));
+        // printf("mySqrtV1(%lf) = %lf\n", (double)i, mySqrtV1(((double)i)));
+        printf("mySqrtV2(%lf) = %lf\n", (double)i, mySqrtV2(((double)i)));
     }
     return 0;
 }
 
-double mySqrt(double n) {
+double mySqrtV1(double n) {
     double guess = n/2;
     double height = n, low = 0;
     do {
@@ -35,5 +37,16 @@ double mySqrt(double n) {
 
     } while(myAbs(myLimit(guess, n)) > LIMIT);
     return guess;
+}
+
+double mySqrtV2(double n) {
+    if(n <= 0){ return 0; }
+    double ret = n;
+    double lst = 0;
+    do {
+        lst = ret;
+        ret = (ret + n/ret)/2;
+    } while(myAbs(ret - lst) > LIMIT);
+    return ret;
 }
 
